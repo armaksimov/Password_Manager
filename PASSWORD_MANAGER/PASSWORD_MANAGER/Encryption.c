a1f9089a8282affc3c66cbbee941a20b8b587805
Artem Maksymov <armaksimov3@gmail.com>
2022-03-14 11:04:55 -0400

    Issues located by test cases for encryption module are now eliminated.
    All test cases, which were earlier created to test functions encrypt() and decrypt() are now successfuly passing, as both functions were changed.

--- PASSWORD_MANAGER/PASSWORD_MANAGER/Encryption.c
+++ PASSWORD_MANAGER/PASSWORD_MANAGER/Encryption.c
@@ -10,16 +10,20 @@ char* encrypt(pApplication app) {
 	char* encrypted = (char*)calloc((APPNAME_LENGTH + USERNAME_LENGTH + PASSWORD_LENGTH) * 2, sizeof(char));
 
 	strncat(encrypted, app->appName, APPNAME_LENGTH);
-	strncat(encrypted, '~', 1);
+	strncat(encrypted, "~", 1);
 
 	for (int i = 0; i < userLength; i++) {
-		strncat(encrypted, *(app->username + i) - 1, 1);
-		strncat(encrypted, *(app->username + i) + 1, 1);
+		char prev = *(app->username + i) - 1;
+		char next = *(app->username + i) + 1;
+		strncat(encrypted, &prev, 1);
+		strncat(encrypted, &next, 1);
 	}
-	strncat(encrypted, '~', 1);
+	strncat(encrypted, "~", 1);
 	for (int i = 0; i < length; i++) {
-		strncat(encrypted, *(app->password + i) - 1, 1);
-		strncat(encrypted, *(app->password + i) + 1, 1);
+		char prev = *(app->password + i) - 1;
+		char next = *(app->password + i) + 1;
+		strncat(encrypted, &prev, 1);
+		strncat(encrypted, &next, 1);
 	}
 
 	return encrypted;
@@ -31,9 +35,13 @@ void decrypt(pApplication app) {
 	int usernameLength = strlen(app->username);
 	int passwordLength = strlen(app->password);
 	for (int i = 0, j = 0; i < usernameLength - 1; i += 2, j++) {
-		decryptedUsername[j] = (app->username[i] + app->username[i + 1]);
+		char decrypted = (app->username[i] + app->username[i + 1]) / 2;
+		strncat(decryptedUsername, &decrypted, 1);
 	}
 	for (int i = 0, j = 0; i < passwordLength; i += 2, j++) {
-		decryptedPassword[j] = (app->password[i] + app->password[i + 1]);
+		char decrypted = (app->password[i] + app->password[i + 1]) / 2;
+		strncat(decryptedPassword, &decrypted, 1);
 	}
+	strncpy(app->username, decryptedUsername, USERNAME_LENGTH);
+	strncpy(app->password, decryptedPassword, PASSWORD_LENGTH);
 }
